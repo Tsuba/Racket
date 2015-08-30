ef97d7cf3fbb58785f1511d2be0450d2095cf91a
Tsuba <Tsuba@gmail.com>
2015-08-30 20:13:15 +0200

    Modifications des mains

--- Raquettes/script/controller/raquetteController.js
+++ Raquettes/script/controller/raquetteController.js
@@ -10,3 +10,13 @@ angular.module('racketApp', [])
         })
 
     }])
+    .controller('panelController',['$scope', function($scope){
+        $scope.tab = 1;
+        $scope.selectTab = function(setTab){
+            $scope.tab = setTab;
+        };
+        $scope.isSelected = function(checkTab){
+            return $scope.tab===checkTab;
+        };
+
+    }])
